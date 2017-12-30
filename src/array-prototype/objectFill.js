7dba7166d2777b673af05b7c726e24930f62f9af
Samu Joseph <samujoseph@outlook.com>
2017-12-30 21:20:23 +0530

    changed objectFill implementation for listed keys

--- src/array-prototype/objectFill.js
+++ src/array-prototype/objectFill.js
@@ -1,10 +1,12 @@
 export const objectFill = (obj, fillValue, ...keys) => {
     if (keys.length) {
-        return Object.entries(obj)
-        .reduce((res, [key, value]) => {
-            res[key] = keys.include(key) ? fillValue : value;
-            return res;
-        }, {});
+        return {
+            ...obj,
+            ...keys.reduce((res, key) => {
+                res[key] = fillValue;
+                return res;
+            }, {}),
+        }
     } 
     return Object.entries(obj)
         .reduce((res, [key]) => {
